951690829252d5b146b879e0009a93bd205331ed
Ant017 <59092146+Ant017@users.noreply.github.com>
2023-06-29 21:58:01 +0600

    AmazeNFT.jsx

--- src/components/amaze-nft/AmazeNFT.jsx
+++ src/components/amaze-nft/AmazeNFT.jsx
@@ -8,14 +8,14 @@ export function AmazeNFT() {
             <div className="overlap-9">
               <p className="part-3-text">The amazing NFT art of the world here</p>
               <div className="part-3-growth">
-                <img className="part-3-chart-square" alt="Chart square" src="/static/img/chart-square-2.svg" />
+                <img className="part-3-chart-square" alt="Chart square" src="static/img/chart-square-2.svg" />
                 <div className="part-3-growth-heading">Growth Transaction</div>
                 <p className="part-3-growth-description">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam etiam viverra tellus
                 </p>
               </div>
               <div className="part-3-fast">
-                <img className="part-3-card-tick" alt="Card tick" src="/static/img/card-tick-1-1.svg" />
+                <img className="part-3-card-tick" alt="Card tick" src="static/img/card-tick-1-1.svg" />
                 <div className="part-3-fast-heading">Fast Transaction</div>
                 <p className="part-3-fast-description">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam etiam viverra tellus imperdiet.
